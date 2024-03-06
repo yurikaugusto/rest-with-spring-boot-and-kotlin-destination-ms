99ebca25c8827de7a59fe4ef7f4d533cb4f3d7cd
Yuri Komesu Augusto <komesu.yuri.a@gmail.com>
2024-03-06 11:15:52 -0300

    Small improvement implemented

--- src/main/kotlin/br/com/yuri/adapters/in/controller/PersonController.kt
+++ src/main/kotlin/br/com/yuri/adapters/in/controller/PersonController.kt
@@ -6,6 +6,7 @@ import br.com.yuri.application.core.domain.PersonDomain
 import br.com.yuri.application.ports.`in`.FindPersonInputPort
 import br.com.yuri.application.ports.`in`.InsertPersonInputPort
 import br.com.yuri.deprecated.model.Person
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,33 +17,30 @@ class PersonController(
     private val findPersonInputPort: FindPersonInputPort
 ) {
 
+    @PostMapping
+    fun create(@RequestBody personRequest: PersonRequest): ResponseEntity<*> {
+        val personDomain = insertPersonInputPort.create(PersonDomain(null, personRequest.firstName, personRequest.lastName, personRequest.address,personRequest.gender))
+        return ResponseEntity(PersonResponse(personDomain.id, personDomain.firstName, personDomain.lastName, personDomain.address,personDomain.gender), HttpStatus.CREATED)
+    }
+
     @GetMapping
-    fun findAll(): List<PersonResponse> {
+    fun findAll(): ResponseEntity<*> {
         val listPerson = findPersonInputPort.findAll()
         val personResponseList = listPerson.map { PersonResponse(it.id, it.firstName, it.lastName, it.address, it.gender) }
-        return personResponseList
+        return ResponseEntity(personResponseList, HttpStatus.OK)
     }
 
     @GetMapping("{id}")
-    fun findById(@PathVariable("id") id: Long): PersonResponse {
+    fun findById(@PathVariable("id") id: Long): ResponseEntity<*> {
         val personDomain = findPersonInputPort.findById(id)
-        return PersonResponse(personDomain.id, personDomain.firstName, personDomain.lastName, personDomain.address, personDomain.gender)
-    }
-
-
-    @PostMapping
-    fun create(@RequestBody personRequest: PersonRequest): PersonResponse {
-        val personDomain = insertPersonInputPort.create(PersonDomain(null, personRequest.firstName, personRequest.lastName, personRequest.address,personRequest.gender))
-        return PersonResponse(personDomain.id, personDomain.firstName, personDomain.lastName, personDomain.address,personDomain.gender)
+        return ResponseEntity(PersonResponse(personDomain.id, personDomain.firstName, personDomain.lastName, personDomain.address, personDomain.gender), HttpStatus.OK)
     }
 
     @PutMapping
     fun update(@RequestBody person: Person): Person = TODO("NOT IMPLEMENTED")
 
-
     @DeleteMapping("/{id}")
     fun delete(@PathVariable("id") id: Long): ResponseEntity<*> {
         TODO("NOT IMPLEMENTED")
     }
-
 }
